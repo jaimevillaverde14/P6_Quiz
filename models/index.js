3b6e70208c244bff56fe66bbf723022450870020
maiteosaba <m.martinezdeosaba@alumnos.upm.es>
2018-05-23 20:07:34 +0200

    p6

--- models/index.js
+++ models/index.js
@@ -9,12 +9,12 @@ const Sequelize = require('sequelize');
 // To use  Heroku Postgres data base:
 //    DATABASE_URL = postgres://user:passwd@host:port/database
 
-const url = process.env.DATABASE_URL || "sqlite:quiz.sqlite";
+const url = process.env.DATABASE_URL || "sqlite:quizzes.sqlite";
 
 const sequelize = new Sequelize(url);
 
 // Import the definition of the Quiz Table from quiz.js
-sequelize.import(path.join(__dirname, 'quiz'));
+sequelize.import(path.join(__dirname, 'quizzes'));
 
 // Session
 sequelize.import(path.join(__dirname,'session'));
